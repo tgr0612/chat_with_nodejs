43eb1a6372797f18322fef7bd29d05d73d0aa04c
tgr0612 <tgr0612@naver.com>
2017-01-31 00:35:40 +0900

    소켓이 연결된 횟수를 console 창에 찍어보자

--- chat.js
+++ chat.js
@@ -3,9 +3,19 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-connections = [];
+connections = []; //소켓 연결된 횟수 알아볼때 쓸거.
 server.listen(process.env.PORT || 3000);
 console.log('3000번 포트에서 서버가 동작 중입니다.');
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
+});
+//소켓이 연결된 횟수를 알아보자.
+io.sockets.on('connection', function(socket){
+  connections.push(socket);//6번째 줄의 connections 배열을 쓰는 거다.
+  console.log('connected : %s sockets connected', connections.length);
+//소켓 연결 해제 되면 connectons.length를 줄이자.
+  socket.on('disconnect', function(){
+    connections.splice(connections.indexOf(socket),1);
+    console.log('disconnected : %s sockets connected', connections.length);
+  });
 });
