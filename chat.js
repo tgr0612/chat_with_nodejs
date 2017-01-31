28399d80b34bc3444167d31bf32a1905d7345682
tgr0612 <tgr0612@naver.com>
2017-01-31 16:04:43 +0900

    에라모르겟다 완성본임

--- chat.js
+++ chat.js
@@ -1,36 +1,40 @@
-//준비물 작성
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-connections = []; //소켓 연결된 횟수 알아볼때 쓸거.
-users=[]; //이름 저장할때 쓸꺼임.
+users = [];
+connections =[];
 server.listen(process.env.PORT || 3000);
-console.log('3000번 포트에서 서버가 동작 중입니다.');
+console.log('Server runnning .....');
 app.get('/', function(req, res){
-  res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname+'/index.html');
 });
-//소켓이 연결된 횟수를 알아보자.
+
 io.sockets.on('connection', function(socket){
-  connections.push(socket);//6번째 줄의 connections 배열을 쓰는 거다.
-  console.log('connected : %s sockets connected', connections.length);
-//소켓 연결 해제 되면 connectons.length를 줄이자.
-  socket.on('disconnect', function(){
-  connections.splice(connections.indexOf(socket),1);
-  console.log('disconnected : %s sockets connected', connections.length);
-  });
-//index.html의  socket.emit('send message', $message.val()); 이벤트를  받는 부분을 만들자.
-  socket.on('send message', function(data){
-  console.log(data);
-  io.sockets.emit('new message', {msg:data, user:socket.username});//다시 new message 이벤트를 보낸다.
-});
-//index.html의 socket.emit('new user', $username.val()); 이벤트를 받는 부분
-  socket.on('new user', function(data){
-  socket.username = data;
-  users.push(socket.username);
-  updateUsernames();
-});
-  function updateUsernames(){
-    io.sockets.emit('get users', users);
-  }
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    //disconnect
+    socket.on('disconnect', function(){
+      if(!socket.username) return;
+      users.splice(users.indexOf(socket.username),1);
+      updateUsernames();
+      connections.splice(connections.indexOf(socket),1);
+      console.log('Disconnected: %s sockets connected',connections.length);
+    });
+    //send Message
+    socket.on('send message', function(data){
+      console.log(data);
+      io.sockets.emit('new message', {msg:data, user:socket.username});
+    });
+    //new user
+    socket.on('new user', function(data, callback){
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsernames();
+    });
+    function updateUsernames(){
+      io.sockets.emit('get users', users);
+    }
 });
